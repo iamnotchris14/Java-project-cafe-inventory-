f617a6e4945042f8ccbe51cbbd3924d04058c583
Jayden <jayden.hoo61@gmail.com>
2025-03-11 15:58:51 +0800

    main

--- Main.java
+++ Main.java
@@ -1,8 +1,34 @@
-public class Main {
+package tutorial2;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("Test");
-	}
+import java.util.Scanner; 
 
-}
+public class CharacterAnalysis { 
+    
+    public static void checkVowelOrConsonant(char ch) { 
+        ch = Character.toLowerCase(ch); 
+        
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
+        { 
+            System.out.println(ch + " is a vowel."); 
+        } else 
+        
+        { 
+            System.out.println(ch + " is a consonant."); 
+        } 
+
+    } 
+
+    public static void main(String[] args)
+    { 
+        Scanner sc = new Scanner(System.in); 
+
+        System.out.print("Enter a character: "); 
+
+        char inputChar = sc.next().charAt(0); 
+
+        checkVowelOrConsonant(inputChar); 
+
+        sc.close(); 
+    
+
+} 
