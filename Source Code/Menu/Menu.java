7003afcd76d3b3453f8fa10878da9ffebd957297
Jayden <jayden.hoo61@gmail.com>
2025-03-12 16:53:59 +0800

    jayden

---
+++ Source Code/Menu/Menu.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+public class Menu {
+    Scanner input = new Scanner(System.in);
+
+    protected String itemId;
+    protected String itemName;
+    protected double itemPrice;
+    protected int itemQty;
+
+    public Menu()
+    {
+
+    }
+
+    public Menu (String itemId, String itemName, double itemPrice , int itemQty )
+    {
+        // this.itemId = itemId;
+        // this.itemName = itemName;
+        // this.itemId = itemId;
+        // this.itemId = itemId;
+
+    }
+
+    public void setMenu(String itemId, String itemName, double itemPrice , int itemQty )
+    {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemQty = itemQty;
+    }
+
+    
+
+}
+
+
+
+
+
+
+
