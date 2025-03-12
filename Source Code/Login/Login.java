4932b63d3e2db07311874ee2d6f47b614e1ce4b4
Siew Wai Choong <siewwaichoong@gmail.com>
2025-03-12 13:39:38 +0800

    login

---
+++ Source Code/Login/Login.java
@@ -0,0 +1,8 @@
+/* filename: Login.java */
+/* subclass of User.Java */
+/* invoke user input */
+
+public class Login extends User
+{
+	
+}
---
+++ Source Code/Login/User.java
@@ -0,0 +1,25 @@
+/* filename: User.java*/
+/* parent class for login*/
+
+public class User
+{
+	protected String username,id;
+	protected int passcode;
+	
+	public User()
+	{
+		
+	}
+	
+	public User(String uName, String uId, int uasscode)
+	{
+		
+	}
+	
+	public void setUser(String uName, String uId, int uPasscode )
+	{
+		
+	}
+	
+	
+}
