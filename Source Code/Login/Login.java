a950f3532df8282fb82ae851552bfcf5f43c2416
Siew Wai Choong <siewwaichoong@gmail.com>
2025-03-13 23:00:37 +0800

    login

--- Source Code/Login/Login.java
+++ Source Code/Login/Login.java
@@ -3,7 +3,49 @@
 /* invoke user input */
 package JavaProjectCafeInventory;
 
-public class Login extends User
+public class Login
 {
-	
+	protected String username,id;
+	protected int passcode;
+		
+	List<Users> listOfUsers = new ArrayList<Users>();
+		
+	listOfUsers.add(new Users("Siew ", "1234"));
+	listOfUsers.add(new Users("Jayden ", "2345"));
+	listOfUsers.add(new Users("JJ ", "3456"));
+	listOfUsers.add(new Users("Chris ", "4567"));
+
+		
+	public User()
+	{
+			
+	}
+		
+	public User(String uName, String uId, int uPasscode)
+	{
+			
+	}
+		
+	public void setUser(String uName, String uId, int uPasscode )
+	{
+		this.username = uName;
+		this.id = uId;
+		this.passcode = uPasscode;
+
+	}
+		
+	public String getName()
+	{
+		return username;
+	}
+		
+	public String getId()
+	{
+		return id;
+	}
+		
+	public int getPasscode()
+	{
+		return passcode;
+	}
 }
